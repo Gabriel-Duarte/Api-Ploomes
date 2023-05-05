6d407e574e26b76e9d130af78d16cf6795737c88
Gabriel Duarte <gabriel.duarte16@hotmail.com>
2023-05-04 21:27:54 -0300

    feat:Criação da conexão com o Sql

---
+++ ApiPloomes/ApiPloomes.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using ApiPloomes.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPloomes.CrossCutting.IoC
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			string mySqlConnection = configuration.GetConnectionString("DefaultConnection");
+			services.AddDbContext<AppDbContext>(options =>
+						 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+						), b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+			return services;
+		}
+	}
+}
