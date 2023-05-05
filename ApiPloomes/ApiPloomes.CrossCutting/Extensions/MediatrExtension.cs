ca17004978ab40736a987a750065ce8a6ca66f2b
Gabriel Duarte <gabriel.duarte16@hotmail.com>
2023-05-05 17:18:52 -0300

    feat: criação da injeção de dependencia do mediator

---
+++ ApiPloomes/ApiPloomes.CrossCutting/Extensions/MediatrExtension.cs
@@ -0,0 +1,19 @@
+﻿using ApiPloomes.Application.Queries;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPloomes.CrossCutting.Extensions
+{
+	public static class MediatrExtension
+	{
+		public static void AddMediatRApi(this IServiceCollection services)
+		{
+			services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblyContaining(typeof(GetProdutosQueryHandler)));
+			
+		}
+	}
+}
--- ApiPloomes/ApiPloomes.CrossCutting/IoC/DependencyInjection.cs
+++ ApiPloomes/ApiPloomes.CrossCutting/IoC/DependencyInjection.cs
@@ -1,10 +1,14 @@
-﻿using ApiPloomes.Infrastructure.Context;
+﻿using ApiPloomes.CrossCutting.Extensions;
+using ApiPloomes.Domain.Interfaces;
+using ApiPloomes.Infrastructure.Context;
+using ApiPloomes.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +23,8 @@ namespace ApiPloomes.CrossCutting.IoC
 			services.AddDbContext<AppDbContext>(options =>
 						 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
 						), b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
-
+			services.AddScoped<IProdutoRepository, ProdutoRepository>();
+			services.AddMediatRApi();
 			return services;
 		}
 	}
