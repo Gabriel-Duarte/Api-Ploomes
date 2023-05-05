b382828019cc956a43557e268fd82765e7e1a185
Gabriel Duarte <gabriel.duarte16@hotmail.com>
2023-05-05 17:19:35 -0300

    feat: Crição das notificações

---
+++ ApiPloomes/ApiPloomes.Application/Events/LogEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using ApiPloomes.Application.Notifications;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPloomes.Application.Events
+{
+	public class LogEventHandler :
+		
+					INotificationHandler<ErrorNotification>
+	{
+
+		public Task Handle(ErrorNotification notification, CancellationToken cancellationToken)
+		{
+			return Task.Run(() =>
+			{
+				Console.WriteLine($"ERROR : '{notification.Error} \n {notification.Stack}'");
+			});
+		}
+	}
+}
