05fdbd101ea0e43837d3562d01138ec6ca37e17a
Gabriel Duarte <gabriel.duarte16@hotmail.com>
2023-05-05 17:14:34 -0300

    fix: Retirada dos DataAnnotations

--- ApiPloomes/ApiPloomes.Domain/Entities/Produto.cs
+++ ApiPloomes/ApiPloomes.Domain/Entities/Produto.cs
@@ -11,19 +11,11 @@ namespace ApiPloomes.Domain.Entities
 	[Table("Produtos")]
 	public class Produto
 	{
-		[Key]
+
 		public int ProdutoId { get; set; }
-		[Required]
-		[StringLength(80)]
 		public string? Nome { get; set; }
-		[Required]
-		[StringLength(300)]
 		public string? Descricao { get; set; }
-		[Required]
-		[Column(TypeName = "decimal(10,2)")]
 		public decimal Preco { get; set; }
-		[Required]
-		[StringLength(300)]
 		public string? ImagemUrl { get; set; }
 		public float Estoque { get; set; }
 		public DateTime DataCadastro { get; set; }
