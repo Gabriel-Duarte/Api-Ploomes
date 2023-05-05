491cb9f83370b8c6a8502fa1f9447dbb830224d9
Gabriel Duarte <gabriel.duarte16@hotmail.com>
2023-05-04 21:25:49 -0300

    feat: Criação das entidades

---
+++ ApiPloomes/ApiPloomes.Domain/Entities/Categoria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPloomes.Domain.Entities
+{
+	[Table("Categorias")]
+	public class Categoria
+	{
+		public Categoria()
+		{
+			Produtos = new Collection<Produto>();
+		}
+		[Key]
+		public int CategoriaId { get; set; }
+		[Required]
+		[StringLength(80)]
+		public string? Nome { get; set; }
+		[Required]
+		[StringLength(300)]
+		public string? ImagemUrl { get; set; }
+		public ICollection<Produto>? Produtos { get; set; }
+	}
+}
