499486f7d64bb5425128934a52422a383d2669aa
Gabriel Duarte <gabriel.duarte16@hotmail.com>
2023-05-05 17:13:45 -0300

    feat: Criação do repository de produtos

---
+++ ApiPloomes/ApiPloomes.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApiPloomes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPloomes.Domain.Interfaces
+{
+	public interface IProdutoRepository
+	{
+		Task<IEnumerable<Produto>> GetProdutosAsync();
+
+	}
+}
---
+++ ApiPloomes/ApiPloomes.Infrastructure/Repositories/ProdutoRepository.cs
@@ -0,0 +1,27 @@
+﻿using ApiPloomes.Domain.Entities;
+using ApiPloomes.Domain.Interfaces;
+using ApiPloomes.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiPloomes.Infrastructure.Repositories
+{
+	public class ProdutoRepository : IProdutoRepository
+	{
+		private AppDbContext _productContext;
+		public ProdutoRepository(AppDbContext context)
+		{
+			_productContext = context;
+		}
+
+		public async Task<IEnumerable<Produto>> GetProdutosAsync()
+		{
+			return await _productContext.Produtos.ToListAsync();
+		}
+
+	}
+}
